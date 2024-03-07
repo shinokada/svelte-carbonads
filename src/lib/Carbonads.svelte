c3b20a80c3a1daaa76dd7406616cc5ebecde1951
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-07 08:41:10 +0100

    initial commit

---
+++ src/lib/Carbonads.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { page, navigating } from '$app/stores';
+	interface WindowWithCarbonAds extends Window {
+		_carbonads: {
+			refresh: () => void;
+		};
+	}
+	export let carbonSrc: string = '';
+	export let adclass: string = 'fixed bottom-5 right-5 z-50 hidden sm:block';
+
+	onMount(() => {
+		refreshCarbonAds();
+	});
+	$: if ($navigating) {
+		refreshCarbonAds();
+	}
+
+	function refreshCarbonAds() {
+		const isCarbonAdsRendered = document.querySelector('#carbonads');
+
+		if (isCarbonAdsRendered) {
+			(window as unknown as WindowWithCarbonAds)._carbonads.refresh();
+		} else {
+			const script = document.createElement('script');
+			script.async = true;
+			script.id = '_carbonads_js';
+			script.src = carbonSrc;
+
+			const container = document.querySelector('#carbon-container');
+			if (container) {
+				container.appendChild(script);
+			}
+		}
+	}
+</script>
+
+<aside class={adclass}>
+	<div id="carbon-container" />
+</aside>
