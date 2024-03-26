6abd7b524996146ab73c81c0a47f71cb725390fb
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-26 07:05:18 +0100

    test: add test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected h1, meta title', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Svelte Carbonads - 4/5/Runes' })).toBeVisible();
+  await expect(page).toHaveTitle('Svelte Carbonads');
 });
