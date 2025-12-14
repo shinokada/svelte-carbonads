69dc3c946aff4d3e60590e09ffdc3843fb4e9e97
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-12-14 16:01:19 +0100

    chore: dependencies update

--- dist/Carbonads.svelte.d.ts
+++ dist/Carbonads.svelte.d.ts
@@ -1,6 +1,6 @@
 interface Props {
-    carbonSrc: string;
-    class?: string;
+  carbonSrc: string;
+  class?: string;
 }
 /**
  * [Go to docs](https://svelte-carbonads.codewithshin.com/)
@@ -8,6 +8,6 @@ interface Props {
  * @prop carbonSrc: string - The Carbon Ads script source URL
  * @prop class?: string - CSS classes for the aside container (default: 'fixed bottom-5 right-5 z-50 hidden sm:block')
  */
-declare const Carbonads: import("svelte").Component<Props, {}, "">;
+declare const Carbonads: import('svelte').Component<Props, {}, ''>;
 type Carbonads = ReturnType<typeof Carbonads>;
 export default Carbonads;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import '../app.css';
   import { Runatics } from 'runatics';
-  import { RunesMetaTags, deepMerge } from 'runes-meta-tags';
+  import { MetaTags, deepMerge } from 'runes-meta-tags';
   import { page } from '$app/state';
   import Nav from './utils/Nav.svelte';
   import Footer from './utils/Footer.svelte';
@@ -9,13 +9,10 @@
   const analyticsId = $derived(data.ANALYTICS_ID);
   // meta tags
   let metaTags = $derived(page.data.pageMetaTags ? deepMerge(page.data.layoutMetaTags, page.data.pageMetaTags) : data.layoutMetaTags);
-  $effect(() => {
-    metaTags = page.data.pageMetaTags ? deepMerge(page.data.layoutMetaTags, page.data.pageMetaTags) : data.layoutMetaTags;
-  });
 </script>
 
 <Runatics {analyticsId} />
-<RunesMetaTags {...metaTags} />
+<MetaTags {...metaTags} />
 
 <Nav />
 <div class="relative mx-auto mt-16 h-full max-w-5xl overflow-y-auto px-8 pb-20">
